d550f7a3d33bc28e24cc5704ae360cbce27d46cd
Matthew_Downs <142461031+Paebak@users.noreply.github.com>
2025-10-04 07:41:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ base: '/', plugins: [react()] })
+// Root apex domain => base MUST be '/'
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',           // default; keep it explicit
+    sourcemap: false
+  }
+})
